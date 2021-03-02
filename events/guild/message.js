f235b3b58dd8d2205e678ae746ff185d2178cb25
Artem-009 <dom.artem2004@gmail.com>
2021-03-02 20:17:52 +0200

    Create message.js

---
+++ events/guild/message.js
@@ -0,0 +1,10 @@
+module.exports = (Discord, client, message, config) => {
+    if(!message.content.startsWith(config.prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const cmd = args.shift().toLowerCase();
+
+    const command = client.commands.get(cmd);
+
+    if (command) command.execute(client, message, args, Discord);
+}
