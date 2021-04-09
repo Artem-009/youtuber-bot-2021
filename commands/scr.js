7db62cff7d47a3b0b50c2d6f79dbdfeb704d95ba
Artem Larchenko <>
2021-04-09 17:30:38 +0000

    scr command again some fixes

--- commands/scr.js
+++ commands/scr.js
@@ -8,10 +8,12 @@ module.exports = {
         const embedScreenshot = new Discord.MessageEmbed()
       .setTitle(`Скриншот сайта ${args[0]}`)
       .setColor("GREEN")
-      .setImage(`http://mini.s-shot.ru/1280x720/JPEG/1280/Z100/https://?${args[0]}`)
+      .setImage(`https://shot.screenshotapi.net/screenshot?&url=${args[0]}&width=1280&height=720&output=image&file_type=png`)
       .setFooter("Команду запросил - " + message.author.tag, `${message.author.displayAvatarURL()}`)
       .setTimestamp();
-      
       message.channel.send(embedScreenshot).then(message.react('<a:check:797107045488001025>'));
+
+      // .setImage(`http://mini.s-shot.ru/1280x720/JPEG/1280/Z100/https://?${args[0]}`)
+      // .setImage(`https://shot.screenshotapi.net/screenshot?&url=${args[0]}&full_page=false&output=image&file_type=png`)
 	},
 };
